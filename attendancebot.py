da3a2a6b1fa213ab185fccd6bee2dfdeeeaa8c7f
bohan.yu <bohan.yu@nextbillion.ai>
2022-03-17 20:29:47 +0800

    Implemented logging

--- attendancebot.py
+++ attendancebot.py
@@ -1,5 +1,13 @@
+import logging
+
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Attendance-bot started')
