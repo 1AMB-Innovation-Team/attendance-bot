6be5de40b4ce99abe3d2c7b74bf5f5e067410edf
bohan.yu <bohan.yu@nextbillion.ai>
2022-04-01 18:51:19 +0800

    Added help command

--- attendancebot.py
+++ attendancebot.py
@@ -12,12 +12,16 @@ logger = logging.getLogger(__name__)
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Attendance-bot started')
 
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Help is coming soon')
+
 def main() -> None:
     """Start the bot."""
     updater = Updater("TOKEN")
 
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
     
     updater.start_polling()
     updater.idle()
